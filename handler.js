8dc6222794591b061ad748f3deead6f797e9d212
vignesh-vl <vigneshl.19cse@kongu.edu>
2022-04-18 11:41:24 +0530

    Project completed!

---
+++ handler.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+
+exports.handler = async (event) => {
+    // TODO implement
+  const city = event.currentIntent.slots["City"];
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=08e3ad0437282f0abefa56ee74ab56af`;
+
+   try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    const answer = "The temperature is " + data.main.temp + "C and Humidity is " + data.main.humidity + "% and " + data.weather[0].description + " is expected.";
+   
+    return {
+      "sessionAttributes": {},
+      "dialogAction": {
+        "type": "Close",
+        "fulfillmentState": "Fulfilled",
+        "message": {
+          "contentType": "PlainText",
+          "content": answer
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
